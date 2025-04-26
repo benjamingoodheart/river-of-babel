b1870aebcf9cea0697bfc592a54564f765cfec37
benjamingoodheart <bgoodheart@fordham.edu>
2025-04-26 12:23:53 -0400

    frontend init

--- rob-backend/index.js
+++ rob-backend/index.js
@@ -1,18 +1,17 @@
 function determine_service(link) {
-  const apple_regex = /(\bappl\w+\b)/;
-  const spotify_regex = /(\bspotif\w+\b)/;
-  const str = link;
-
-  if (str.search(apple_regex) == 0) {
-    console.log("apple");
-  } else if (str.search(spotify_regex) == 0) {
-    console.log("spotify");
-  } else {
-    console.log(-1);
+    const apple_regex = /(\bappl\w+\b)/;
+    const spotify_regex = /(\bspotif\w+\b)/;
+    const str = link;
+  
+    if (str.search(apple_regex) !=-1) {
+      console.log("apple");
+    } else if (str.search(spotify_regex) != -1) {
+      console.log("spotify");
+    } else {
+      console.log(-1);
+    }
   }
-}
 
-var apple_link = "apple.com/us/musicbasd";
-var spotify_link = "spotify.com/whatever";
+  apple_link = "https://music.apple.com/us/album/haunted-cities/1485061880"
 
-determine_service(apple_link);
+  determine_service(apple_link)
---
+++ rob-frontend/app/pages/index.vue
@@ -0,0 +1,36 @@
+import { USeparator, UContainer, UInput } from '../../.nuxt/components';
+<script setup lang="ts">
+const value = ref('')
+function determine_service(link) {
+  const apple_regex = /(\bappl\w+\b)/g;
+  const spotify_regex = /(\bspotif\w+\b)/;
+  const str = link;
+
+  if (str.search(apple_regex) !=-1) {
+    console.log("apple");
+  } else if (str.search(spotify_regex) != -1) {
+    console.log("spotify");
+  } else {
+    console.log(-1);
+  }
+}
+
+function convert(){
+  console.log("run convert process")
+}
+</script>
+<template>
+  <UContainer>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="font-bold text-2xl text-(--ui-primary)">
+      River of Babel
+    </h1>
+    <USeparator/>
+    <UInput placeholder="Paste Your Link Here" v-model="value" :onchange="determine_service(value)" />
+    <UButton :onclick="convert">Convert To Service Link</UButton>
+    <UCard>
+{{value}}
+    </UCard>
+  </div>
+</UContainer>
+</template>

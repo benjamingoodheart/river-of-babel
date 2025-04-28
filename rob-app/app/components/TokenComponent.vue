25c6f0818a312f97926e607d604761479f2b40ee
benjamingoodheart <bgoodheart@fordham.edu>
2025-04-27 21:34:13 -0400

    spotify token

--- rob-app/app/components/TokenComponent.vue
+++ rob-app/app/components/TokenComponent.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-const {data} = await useFetch('/api/getJWT')
-console.log(data)
+// const {data} = await useFetch('/api/getJWT')
+// console.log(data)
+const {data} = await useFetch('/api/getSpotifyToken')
+console.log(await data.value)
+
 </script>
 <template>
 
---
+++ rob-app/server/api/getSpotifyToken.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import {ref} from 'vue'
+export default defineEventHandler(async (event)=>{
+    const endpoint = 'https://accounts.spotify.com/api/token'
+    let response = ref('')
+    await axios.post(
+        endpoint, {
+            'grant_type':'client_credentials',
+            'client_id': process.env.VITE_SPOTIFY_CLIENT_ID,
+            'client_secret': process.env.VITE_SPOTIFY_CLIENT_SECRET,
+    
+        }, {headers: {
+            'Content-Type':'application/x-www-form-urlencoded'
+        }}
+    ).then((res)=>{
+        response.value = res.data
+        console.log(res.data.access_token)
+       
+    }) 
+    if (response.value != '') {
+        return response.value.access_token
+    }
+})

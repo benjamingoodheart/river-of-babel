dca170f9d821897f58bf0f9ca88cac9f8015fa51
benjamingoodheart <bgoodheart@fordham.edu>
2025-06-07 19:37:23 -0400

    added bmac link

--- app/components/FooterComponent.vue
+++ app/components/FooterComponent.vue
@@ -2,6 +2,7 @@
     <UContainer class="flex flex-col items-center justify-center gap-2 mt-7">
         <USeparator/>
     <p class="text-xs mt-5">Created by <a href="https://benjamin.goodheart.dev" target="_blank" class="hover:text-primary-300">Benjamin Goodheart</a></p>
+    <p class="text-xs text-center w-sm"> Support River of Babel by <a href="https://buymeacoffee.com/ereg8e6dr6" target="_blank" class="hover:text-primary-300">buying me a coffee! <UIcon name="material-symbols:coffee-outline" class="size-2 my-0.1"></UIcon> </a> </p>
     <UButton class="text-xs" variant="link" href="https://github.com/benjamingoodheart/river-of-babel/releases" target="blank">v0.1.0</UButton>
     </UContainer>
 </template>

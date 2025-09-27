0d7553951b7e42dba055c9861d4ee09333024517
benjamingoodheart <bgoodheart@fordham.edu>
2025-09-27 13:51:01 -0400

    getting there

--- app/components/RecentLinksComponent.vue
+++ app/components/RecentLinksComponent.vue
@@ -2,7 +2,14 @@
 import { useStorage } from '@vueuse/core'
 
 const recentLinksStorage = useStorage('recent-links')
-
+const recentKey = ref(0)
+const clearDisabled = computed(()=>{
+    if(recentLinksObj.links){
+        return true
+    } else{
+        return false
+    }
+})
 const currentIndex = ref('')
 
 function copy(link, index) {
@@ -19,26 +26,31 @@ function clear(){
 }
 
 const recentLinksObj = computed(()=> {
+    //may be some async thing
+    console.log(recentLinksStorage._rawValue)
     if(recentLinksStorage._rawValue){
     return JSON.parse(recentLinksStorage._rawValue)}
     else{
-        return {}
+        return JSON.stringify(null)
     }
 })
 
-watch(recentLinksStorage,()=>{
+watch(recentLinksStorage, async()=>{
+    recentKey.value = recentKey.value + 1
     console.log('changed')
+    await nextTick()
+    console.log(recentLinksObj)
 })
 </script>
 <template>
-<UPopover>
-    <UButton label="Recent Links" color="neutral" variant="subtle" trailing-icon="i-lucide-chevron-up"/>
-    <template #content>
+<UPopover >
+    <UButton label="Recent Links" color="neutral" variant="subtle" trailing-icon="i-lucide-chevron-up" v-if="recentLinksObj.links"/>
+    <template #content :key="recentKey.value">
       <UCard>
         <div class="grid">
             <div class="grid grid-cols-3">
                 <div class="col-span-2"></div>
-                <div class="col-span-1"> <UButton size="sm" variant="outline" color="info" @click="clear" >Clear</UButton></div>
+                <div class="col-span-1"> <UButton size="sm" variant="outline" color="info" @click="clear()" :disabled="clearDisabled">Clear</UButton></div>
             </div>
             <div class="row-span-12" v-for="(link,index) in recentLinksObj.links"> 
                 <UButton variant="link" class="transition-discrete" icon="material-symbols:content-copy-outline" @click="copy(link,index)" v-if="index!=currentIndex"> <span > {{recentLinksObj.links[link]}} {{link.artists[0]}} - {{link.release}} </span> </UButton>
